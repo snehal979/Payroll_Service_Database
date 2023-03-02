ed3e6e15b42ebfdbc016c481aef1669410bfdc43
Snehal979 <bansodsnehal535@gmail.com>
2023-03-03 01:26:36 +0530

    [Snehal]Add:Create table

--- PayRollServiceDatabase.sql
+++ PayRollServiceDatabase.sql
@@ -1,2 +1,10 @@
 --Uc1 Create Database
 Create database PayRoll_Service_Database
+
+--Uc2 Create table
+Create Table employee_payroll
+(Id int identity(1,1) not null,
+Name varchar(100) not null,
+Salary float,
+start date not null
+)
