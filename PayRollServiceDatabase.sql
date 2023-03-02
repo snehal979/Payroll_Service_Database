399c0865f3aed6c447f4e37ac1adb5f142f2c8fd
Snehal979 <bansodsnehal535@gmail.com>
2023-03-03 01:31:24 +0530

    [Snehal]Add:Insert data in table

--- PayRollServiceDatabase.sql
+++ PayRollServiceDatabase.sql
@@ -8,3 +8,6 @@ Name varchar(100) not null,
 Salary float,
 start date not null
 )
+
+--Uc3 Insert Data
+Insert into employee_payroll Values('Snehal',1000000.00,'2021-01-01'),('Mayur',1500000.00,'2020-04-01'),('Vaibhav',2000000.00,'2018-05-06');
