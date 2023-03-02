73c0ef07bd5dd3460fb3430d2ad87773f302342b
Snehal979 <bansodsnehal535@gmail.com>
2023-03-03 01:34:33 +0530

    [Snehal]Add:Display data in table

--- PayRollServiceDatabase.sql
+++ PayRollServiceDatabase.sql
@@ -11,3 +11,8 @@ start date not null
 
 --Uc3 Insert Data
 Insert into employee_payroll Values('Snehal',1000000.00,'2021-01-01'),('Mayur',1500000.00,'2020-04-01'),('Vaibhav',2000000.00,'2018-05-06');
+
+--Uc4 Display
+Select * From employee_payroll
+Select * From employee_payroll where Id =1;
+Select Name From employee_payroll where Id =1;
